bbb049be9ff73a6b031beeb7c4692f5aed10604c
HaseevAhmad <ihaseev@gmail.com>
2023-04-03 05:46:41 +0600

    M2

---
+++ Module 2/Conditional_1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d",&tk);
+    if (tk == 100)
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+
+    }
+
+    return 0;
+}
