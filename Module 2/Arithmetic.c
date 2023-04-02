51fe8bec67eec3e92f94e8bb12c0511e302b32f7
HaseevAhmad <ihaseev@gmail.com>
2023-04-03 04:03:26 +0600

    M2

---
+++ Module 2/Arithmetic.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+    // int a=10, b=20;
+    // int sum=a+b;
+    // printf ("%d", sum);
+
+    int a=20, b=10;
+    int mns=a-b;
+    printf("%d", mns);
+
+    return 0;
+}
