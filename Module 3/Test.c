e96adf4c16da5a89d96e08a5257fab67bd44bf8d
HaseevAhmad <ihaseev@gmail.com>
2023-04-09 15:47:29 +0600

    M5

---
+++ Module 3/Test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    if (N > 1000) {
+        printf("I will buy Punjabi\n");
+        N -1000= 500;
+        if (N >= 500) {
+            printf("I will buy new shoes\nAlisa will buy new shoes\n");
+        } 
+    } else {
+        printf("Bad luck!\n");
+    }
+    return 0;
+}
